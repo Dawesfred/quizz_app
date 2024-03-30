1f46129d14becdcb1a4344b68419f7b1434024ed
dawesfred <dawesfredrick@gmail.com>
2024-03-30 20:59:32 +0100

    first commit

---
+++ src/Quiz.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { resultInitialState } from "./constants";
+
+const quiz = ({questions}) => {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [answerIdx, setAnswerIdx] = useState(null);
+    const [answer, setAnswer] = useState(null);
+    const [result, setResult] = useState(resultInitialState);
+
+
+    const {question, choices, correctAnswer} = questions[currentQuestion];
+
+    const onAnswerClick = (answer, index) => {
+        setAnswerIdx(index);
+        if(answer === correctAnswer) {
+            setAnswer(true);
+        }else{
+            setAnswer(false);
+        }
+    };
+
+    const onClickNext = () => {
+        setAnswerIdx(null);
+    }
+
+    return (
+        <div className="quiz-container">
+            <>
+            <span className="active-question-no">{currentQuestion +1}</span>
+            <span className="total-question">/{questions.length}</span>
+            <h2>{question}</h2>
+            <ul>
+                {
+                    choices.map((answer, index) => (
+                        <li 
+                            onClick={() => onAnswerClick(answer, index)}
+                            key={answer}
+                            className={answerIdx === index ? 'selected-answer' : null}
+                            >
+                                {answer}
+                        </li>
+                    ))
+                }
+
+            </ul>
+            <div className="footer">
+                <button onClick={onClickNext} disabled={answerIdx === null}>
+                    {currentQuestion === questions.length - 1 ? "Finish" : "Next"}
+                </button>
+            </div>
+            </>
+        </div>
+    );
+}
+
+export default quiz;
