4dd22f69f9b5dfbad53c97d97e983823b653d289
dawesfred <dawesfredrick@gmail.com>
2024-03-31 18:56:44 +0100

    The result is displayed together with the total number of questions

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -42,6 +42,11 @@ const quiz = ({questions}) => {
         }
     };
 
+    const onTryAgain = () => {
+        setResult(resultInitialState);
+        setShowResult(false);
+    }
+
     return (
         <div className="quiz-container">
             {!showResult ? (            <>
@@ -69,6 +74,19 @@ const quiz = ({questions}) => {
             </div>
             </>) : <div className="result">
                 <h3>Result</h3>
+                <p>
+                    Total Questions: <span>{questions.length}</span>
+                </p>
+                <p>
+                    Total Score: <span>{result.score}</span>
+                </p>
+                <p>
+                    Correct Answers: <span>{result.correctAnswers}</span>
+                </p>
+                <p>
+                    Wrong Answers: <span>{result.wrongAnswers}</span>
+                </p>
+                <button onClick={onTryAgain}>Try again</button>
                 </div>}
 
         </div>
